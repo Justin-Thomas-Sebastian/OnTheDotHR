23c0c42073610090600606e423f69a6039b0efb5
Justin Sebastian <justinthomassebastian@gmail.com>
2022-05-06 02:29:51 -0600

    Style progress on landing page and header fragment

---
+++ src/main/resources/static/js/sidepanel.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.getElementById("header-side-panel").style.width = "250px";
+}
+
+/* Set the width of the sidebar to 0 (hide it) */
+function closeNav() {
+    document.getElementById("header-side-panel").style.width = "0";
+}
